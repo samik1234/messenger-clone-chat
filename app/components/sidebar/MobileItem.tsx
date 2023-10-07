6dcf900f3918b7f9f81de3032a707183b27abeed
samiksha <samikshawankhade11@gmail.com>
2023-10-07 20:24:25 +0530

    fix

--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -41,7 +41,7 @@ const MobileItem: React.FC<MobileItemProps> = ({
       `,
         active && 'bg-gray-100 text-black'
       )}>
-      <Icon className="h-6 w-6" />
+      <Icon className="h-6 w-6"/>
     </Link>
   );
 }
